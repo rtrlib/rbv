07bee068ba25e7b5ff064cee111a44f1a4fdb066
smlng <s@mlng.net>
2015-05-27 17:24:25 +0200

    added apache wsgi.conf example

--- src/settings.py
+++ src/settings.py
@@ -1,18 +1,22 @@
-# server environment
+# environment
 rbv_base_path = "."
 validator_path = rbv_base_path + "/util/cli-validator"
 
-# settings
+# connections
 default_cache_server = {"host":"rpki-validator.realmv6.org", "port":"8282"}
 bgp_validator_server = {"host":"localhost", "port":"6414"}
 www_validator_server = {"host":"localhost", "port":"5000"}
 
+# maintenance
 maintenance_timeout = 300
 thread_timeout = maintenance_timeout*5
 thread_errors = 42
+
+# output
 logging = True
 verbose = True
 warning = True
+
 # log validation request/queries, file format:
 #   ClientIP;ClientOS;ClientBrowser;RequestURL;CacheServer;Prefix;ASN;Validity
-validation_log = {"enabled":True, "file":"validation.log"}
+validation_log = {"enabled":True, "file": rbv_base_path + "/validation.log"}
